99863029038f9621b0c5b9f677088f72fdae4bba
Pragathiswaran <mpragathiswaran@gmail.com>
2024-03-10 09:19:39 +0530

    commit

--- lang/en/local_attendance.php
+++ lang/en/local_attendance.php
@@ -1,8 +1,28 @@
 <?php
+// This file is part of Moodle Course Rollover Plugin
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     local_attendance
+ * @author      Prgathiswaran
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Enhanced Attendance Tracking';
-$string['activityreport'] = 'Comprehensive Activity Report';
+$string['report'] = 'Comprehensive Activity Report';
 $string['userid'] = 'User Identification Number';
 $string['username'] = 'User Name';
 $string['coursename'] = 'Name of the Course';
--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_attendance';
-$plugin->version = 2020110902;
+$plugin->version = 2020110903;
 $plugin->requires = 2016052300; // Moodle version
 
