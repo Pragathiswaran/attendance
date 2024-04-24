c6177ebb2b922e88ee829ce94e6982382fddb4c5
Rukkesh <gkrukkesh@gmail.com>
2024-04-24 16:07:21 +0530

    ch

--- chart.php
+++ chart.php
@@ -1,51 +1,103 @@
 <?php
-// This file is part of Moodle Course Rollover Plugin
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+require_once(__DIR__.'/../../config.php');
+require_once($CFG->libdir . '/formslib.php');
 
-/**
- * @package     local_attendance
- * @author      Prgathiswaran
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @var stdClass $plugin
- */
+echo $OUTPUT->header();
+echo $OUTPUT->heading('User Moodle Access Duration Chart');
 
- require_once(__DIR__.'/../../config.php'); 
+class select_user_form extends moodleform {
+    protected function definition() {
+        $mform = $this->_form;
+        
+        // Fetch non-admin users
+        $users = get_non_admin_users();
+        $mform->addElement('select', 'userid', 'Select User:', $users);
+        $mform->setType('userid', PARAM_INT);
+        $mform->addElement('submit', 'submitbutton', 'Show Data');
+    }
+}
 
- echo $OUTPUT->header();
-    echo $OUTPUT->heading('Chart Example');
-//  $sales = new core\chart_series('Sales',[100,200,300,400,500,600,700,800,900,1000]);
-// $expenses = new core\chart_series('Expenses',[50,100,150,200,250,300,350,400,450,500]);
-// $labels = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct'];
-// $chart = new core\chart_pie();
-// // $chart = new core\set_smooth(true);
-// // $chart->add_series($sales);
-// $chart->add_series($expenses);
-// $chart->set_labels($labels);
-// echo $OUTPUT->render($chart);
+function get_non_admin_users() {
+    global $DB;
+    $users = [];
+    $all_users = $DB->get_records_sql("SELECT * FROM {user} WHERE deleted = 0 AND suspended = 0 AND id <> 2 AND id <> 1");
+    foreach ($all_users as $user) {
+        if (!in_array($user->id, $admin_ids)) {
+            $users[$user->id] = fullname($user);
+        }
+    }
+    return $users;
+}
 
-// $xaxis = new chart_axis();
-// $axis->set_stepsize(5);
-$chart = new \core\chart_pie(); // Create a bar chart instance.
-$chart -> set_doughnut(true);
+function fetch_user_access_data($userid) {
+    global $DB;
+    $sql = "SELECT FROM_UNIXTIME(timecreated) as timecreated, action 
+            FROM {logstore_standard_log} 
+            WHERE userid = :userid AND action IN ('loggedin', 'loggedout')
+            ORDER BY timecreated ASC";
+    $params = ['userid' => $userid];
+    $records = $DB->get_records_sql($sql, $params);
 
-$series1 = new \core\chart_series('Series 1 (Bar)', [1000, 1170, 660, 1030]);
-$series2 = new \core\chart_series('Series 2 (Line)', [400, 460, 1120, 540]);
-// $series2->set_type(\core\chart_series::TYPE_LINE); // Set the series type to line chart.
-$chart->add_series($series2);
-// $chart->add_series($series1);
-$chart->set_labels(['2004', '2005', '2006', '2007']);
-echo $OUTPUT->render($chart);
+    $durations = [];
+    $lastLogin = null;
+
+    foreach ($records as $record) {
+        if ($record->action == 'loggedin') {
+            $lastLogin = new DateTime($record->timecreated);
+        } elseif ($record->action == 'loggedout' && $lastLogin !== null) {
+            $logoutTime = new DateTime($record->timecreated);
+            $duration = $logoutTime->getTimestamp() - $lastLogin->getTimestamp();
+            $date = $lastLogin->format('Y-m-d');
+            if (!isset($durations[$date])) {
+                $durations[$date] = 0;
+            }
+            $durations[$date] += $duration;
+            $lastLogin = null;
+        }
+    }
+
+    return $durations;
+}
+
+$form = new select_user_form();
+
+if ($form->is_cancelled()) {
+    // Handle form cancellation
+} elseif ($fromform = $form->get_data()) {
+    $selected_user_id = $fromform->userid;
+    $durations = fetch_user_access_data($selected_user_id);
+
+    $labels = array_keys($durations);
+    $values = array_values($durations);
+
+    // Convert seconds to hours
+    $values = array_map(function ($sec) { return round($sec / 3600, 2); }, $values);
+
+    // Create series for the bar chart
+    $access = new \core\chart_series('Daily Access Duration (hours)', $values);
+
+    // Initialize a new bar chart
+    $chart = new \core\chart_bar();
+    $chart->add_series($access);
+    $chart->set_labels($labels);
+
+    // Correctly create and set the axes
+    $xaxis = new \core\chart_axis();
+    $xaxis->set_label('<------- Date ------->');
+    $chart->set_xaxis($xaxis);
+
+    $yaxis = new \core\chart_axis();
+    $yaxis->set_label('<----- Duration in Hours ----->');
+    $chart->set_yaxis($yaxis);
+
+    // Render the chart inside a container
+    echo '<div class="chart-container">';
+    echo $OUTPUT->render($chart);
+    echo '</div>';
+
+} else {
+    // Display the form
+    $form->display();
+}
 
 echo $OUTPUT->footer();
